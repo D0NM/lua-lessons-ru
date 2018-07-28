44dac28d4ef26508f57cfd54ea309dad0b573fee
D0NM <hyuBYSnONMVWgWDpCeV4>
2018-07-28 23:53:25 +0500

    initial 5th sprite lesson

---
+++ Sprites/sprites lesson 5 aseprite/main.lua
@@ -0,0 +1,24 @@
+-- Библиотека анимации peachy (анимация из редактора aseprite) by Josh Perry
+-- УРОК 5
+local peachy = require("peachy")
+
+function love.load()
+  love.window.setTitle("Peachy example")
+  spriteSheet = love.graphics.newImage("res/spr/astst.png")
+
+  jumpy = peachy.new("res/spr/astst.json", spriteSheet, "Jumpy")
+
+end
+
+function love.draw()
+  jumpy:draw(50, 80)
+end
+
+function love.update(dt)
+  jumpy:update(dt)
+end
+
+function love.keypressed(key)
+  if key == "space" then
+  end
+end
