6c8e5c514a0a66d1bf3c87f04f530f427a8253ce
Mikhail Bratus <mikhail.bratus@gmail.com>
2018-10-05 01:17:41 +0500

    урок 1 по исполнителю Черепашка на Lua (автор модуля Paul Kluchenko)

---
+++ turtle/lesson1.lua
@@ -0,0 +1,19 @@
+require "turtle"
+
+pnsz(3)
+
+local ребро = 150
+pncl(ranc())
+move(ребро)
+turn(90)
+pncl(ranc())
+move(ребро)
+turn(90)
+pncl(ranc())
+move(ребро)
+turn(90)
+pncl(ranc())
+move(ребро)
+turn(90)
+pncl(ranc())
+wait()
